a63d4eb7182e033baf09c408ddaeafad16a427a9
Jeff Stafford <jeff.stafford@live.com>
2015-04-18 19:08:51 -0700

    it's finally accepting my function definitions

--- R/DAMclass.R
+++ R/DAMclass.R
@@ -1,18 +1,33 @@
-# This sets up the DAM experiment class
+# This sets up the DAM experiment class and its native methods.
 
-setClass("DAM",
-         slots = c(data = "data.frame",
-                   sample_info = "data.frame"))
+setClass("DAM", slots = c(data = "data.frame", sample_info = "data.frame"))
 
 newExperiment <- function(dataFile = NULL, infoFile = NULL) {
+  if (is.null(dataFile) || is.null(infoFile)) {
+    warning("One or more arguments is missing from DAM constructor.")
+  }
   new("DAM", data = dataFile, sample_info = infoFile)
 }
 
-# A method to combine experiments
-# setMethod("catExperiments", signature = "DAM",
-#           #valueClass = "DAM",
-#           valueClass = "data.frame",
-#           definition = function(exp1, exp2) {
-#             exp1@data
-#             }
-#           )
+# Subset out flies by sex.
+setGeneric("bySex", function(obj, sex) {standardGeneric("bySex")})
+setMethod(f = "bySex", signature = "DAM",
+          definition = function(obj, sex = c("female", "male")) {
+            sex <- match.arg(sex)
+            if (sex == "female") {
+              sex <- 0
+            } else {
+              sex <- 1
+            }
+
+            # Some really dirty subsetting....
+            idx <- which(obj@sample_info$sex == sex)
+            obj@sample_info <- obj@sample_info[idx, ]
+            lightsCol <- which(colnames(obj) == "light_status")
+            offsetIdx <- lightsCol + 1 + idx
+            obj@data <- obj@data[c(1:lightsCol, idx), ]
+
+            return(obj)
+          })
+
+
--- R/subsetTime.R
+++ R/subsetTime.R
@@ -1,19 +1,22 @@
 # Use this function to subset out a particular portion of an experiment (to clip
 # off unused data from beginnning and end).
 
-subsetData <- function(DAMdfr, startTime = 0, expDuration = dim(DAMdfr)[1],
+subsetTime <- function(DAMdfr, startTime = 0, expDuration = dim(DAMdfr)[1],
                        units = c("seconds", "minutes", "hours")) {
+  # Parse them arguments...
   units <- match.arg(units)
   startTime <- toSeconds(startTime, units)
   expDuration <- toSeconds(expDuration, units)
   interval <- getInterval(DAMdfr)
 
+  # Fix bad values to aid indexing.
   if (!is.integer(startTime / interval)) {
     warning("startTime is not a multiple of data interval, coercing to integer.")
+    startTime <- floor(startTime / interval) * interval
   }
-
   if (!is.integer(expDuration / interval)) {
     warning("expDuration is not a multiple of data interval, coercing to integer.")
+    expDuration <- floor(expDuration / interval) * interval
   }
 
   # Okay subset out and return the data we want.
