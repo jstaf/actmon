82328b6a4f5e196a100b099d651c0a9abd40004d
Jeff Stafford <jeff.stafford@live.com>
2015-04-18 21:12:05 -0700

    DAM class methods for extracting/dropping attributes, combining experiments

--- R/DAMclass.R
+++ R/DAMclass.R
@@ -9,25 +9,71 @@ newExperiment <- function(dataFile = NULL, infoFile = NULL) {
   new("DAM", data = dataFile, sample_info = infoFile)
 }
 
-# Subset out flies by sex.
-setGeneric("bySex", function(obj, sex) {standardGeneric("bySex")})
-setMethod(f = "bySex", signature = "DAM",
-          definition = function(obj, sex = c("female", "male")) {
-            sex <- match.arg(sex)
-            if (sex == "female") {
-              sex <- 0
-            } else {
-              sex <- 1
-            }
+# A more generic function to subset your data by whichever column you freaking
+# want. Even works on vectors, so you can subset by multiple values of an
+# attribute at a time.
+setGeneric("byAttribute", function(obj, string, col) {standardGeneric("byAttribute")})
+setMethod(f = "byAttribute", signature = "DAM",
+          definition = function(obj, string, col) {
+            #figure out which column in the info file to read from
+            col <- as.character(col)
+            numCol <- which(colnames(obj@sample_info) == col)
 
-            # Some really dirty subsetting....
-            idx <- which(obj@sample_info$sex == sex)
+            # subset sampleinfo
+            string <- as.character(string)
+            idx <- which(obj@sample_info[, numCol] %in% string)
             obj@sample_info <- obj@sample_info[idx, ]
-            lightsCol <- which(colnames(obj) == "light_status")
-            offsetIdx <- lightsCol + 1 + idx
-            obj@data <- obj@data[c(1:lightsCol, idx), ]
+
+            # subset data
+            lightsCol <- which(colnames(obj@data) == "light_status")
+            offsetIdx <- lightsCol + idx
+            obj@data <- obj@data[, c(1:lightsCol, offsetIdx)]
 
             return(obj)
           })
 
+# Equivalent to byAttribute(), but drops data by a value instead. Works on vectors.
+setGeneric("dropAttribute", function(obj, string, col) {standardGeneric("dropAttribute")})
+setMethod(f = "dropAttribute", signature = "DAM",
+          definition = function(obj, string, col) {
+            #figure out which column in the info file to read from
+            col <- as.character(col)
+            numCol <- which(colnames(obj@sample_info) == col)
 
+            # subset sampleinfo
+            string <- as.character(string)
+            idx <- which(obj@sample_info[, numCol] %in% string)
+            obj@sample_info <- obj@sample_info[-idx, ]
+
+            # subset data
+            lightsCol <- which(colnames(obj@data) == "light_status")
+            offsetIdx <- lightsCol + idx
+            obj@data <- obj@data[, -offsetIdx]
+
+            return(obj)
+          })
+
+# Use this function to mash experiments together. There is no restriction to
+# doing this by timestamp, etc. - so be careful what you combine. The vial
+# indices in the second experiment are relabeled to start where the first
+# experiment stops.
+setGeneric("catExperiments", function(obj1, obj2) {standardGeneric("catExperiments")})
+setMethod(f = "catExperiments", signature = "DAM",
+          definition = function(obj1, obj2) {
+            # retrieve last vial# in obj1
+            last <- colnames(obj1@data)[length(colnames(obj1@data))]
+
+            # rename all vial#s in obj2
+            numVials <- length(obj2@sample_info[, 1])
+            obj2@sample_info[, 1] <- (last + 1):numVials
+
+            lightsCol <- which(colnames(obj2@data) == "light_status")
+            colnames(obj2@data)[lightsCol + 1:numVials] <-
+              (last + 1):numVials
+
+            # combine objects
+            obj1@data <- cbind(obj1@data, getVals(obj2@data))
+            obj1@sample_info <- rbind(obj1@sample_info, obj2@sample_info)
+
+            return(obj1)
+          })
