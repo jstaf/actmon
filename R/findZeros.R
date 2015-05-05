0829bb94b88ed17aa61a79e2cbbe20dc1547540a
Jeff Stafford <jeff.stafford@live.com>
2015-05-05 16:05:03 -0700

    lab meeting!

--- R/findZeros.R
+++ R/findZeros.R
@@ -3,6 +3,42 @@
 setGeneric("findLightInterval", function(DAM) {standardGeneric("findLightInterval")})
 setMethod("findLightInterval", signature = "DAM",
           definition = function(DAM) {
+            diff <- getLightChanges(DAM)
+            
+            # remove the first and last values for a cleaner average
+            diff <- diff[2:(length(diff) - 1)]
+            # take the geometric mean and find which side of the range it's closest to
+            avg <- (prod(diff)) ^ (1 / length(diff))
+            rng <- range(diff)
+            ind <- which.min(abs(rng - avg))
+            
+            hours <- rep(NA, 10000)
+            seed <- 100
+            for (i in round(avg):rng[ind]) {
+              hours[i] <- difftime(DAM@data$read_time[seed + i],
+                                   DAM@data$read_time[seed],
+                                   units = "hours")
+            }
+            hours <- hours[is.na(hours) == FALSE]
+            # retrieve even numbers of hours
+            ans <- hours[round(hours) == hours]
+            return(ans)
+          })
+
+setGeneric("syncLightCycle", function(DAM, hourPerCycle, completeCycle, lightFirst) {
+  standardGeneric("syncLightCycle")
+  })
+setMethod("syncLightCycle", signature = "DAM",
+          definition = function(DAM, hourPerCycle,
+                               completeCycle = TRUE, lightFirst = TRUE) {
+            diff <- getLightChanges(DAM)
+            
+            return(subsetTime(DAM, start, end, "hours") )
+          })
+
+setGeneric("getLightChanges", function(DAM) {standardGeneric("getLightChanges")})
+setMethod("getLightChanges", signature = "DAM",
+          definition = function(DAM) {
             changed <- whichChanged(DAM@data$light_status, 0)
             
             # find difference in indices
@@ -37,28 +73,8 @@ setMethod("findLightInterval", signature = "DAM",
               }
             }
             # remove the small value streaks from diff
-            diff <- diff[-idx]
-            # remove the first and last values for a cleaner average
-            diff <- diff[2:(length(diff) - 1)]
-            # take the geometric mean and find which side of the range it's closest to
-            avg <- (prod(diff)) ^ (1 / length(diff))
-            rng <- range(diff)
-            ind <- which.min(abs(rng - avg))
-            
-            hours <- rep(NA, 10000)
-            seed <- 100
-            for (i in round(avg):rng[ind]) {
-              hours[i] <- difftime(DAM@data$read_time[seed + i],
-                                   DAM@data$read_time[seed],
-                                   units = "hours")
-            }
-            hours <- hours[is.na(hours) == FALSE]
-            # retrieve even numbers of hours
-            ans <- hours[round(hours) == hours]
-            return(ans)
+            return(diff[-idx])
           })
-
-
 
 # find indices of which values which differed by more than a certain delta
 whichChanged <- function(vector, delta) {
