532697824102237165579f96ef211c42e088bfd0
Jeff Stafford <jeff.stafford@live.com>
2015-05-10 01:34:41 -0700

    barplot now has extra fancy

--- R/plotting_fns.R
+++ R/plotting_fns.R
@@ -22,54 +22,62 @@ setMethod("prepPlot", signature = "DAMstats",
             return(list(meta, plotData))
           })
 
-# makes a barplot (dodged if using multiple timepoints)
-barPlot <- function(dataList) {
-  meta <- dataList[[1]]
-  plotData <- dataList[[2]]
-  
-  gg <- ggplot2::ggplot(plotData, ggplot2::aes(x = attrib,
-                                               y = AVG,
-                                               ymin = AVG - SEM,
-                                               ymax = AVG + SEM,
-                                               fill = attrib)) +
-    ggplot2::geom_bar(stat = "identity", width = 0.8) +
-    ggplot2::geom_errorbar(width = 0.4) +
-    ggplot2::guides(fill = FALSE) +
-    ggplot2::theme_bw()
-}
-
+# makes a barplot (faceted if using multiple timepoints)
+setGeneric("barPlot", function(statsObj) {standardGeneric("barPlot")})
+setMethod("barPlot", signature = "DAMstats", 
+          definition = function(statsObj) {
+            dataList <- prepPlot(statsObj)
+            meta <- dataList[[1]]
+            plotData <- dataList[[2]]
+            
+            gg <- ggplot2::ggplot(plotData, ggplot2::aes(x = attrib,
+                                                         y = AVG,
+                                                         ymin = AVG - SEM,
+                                                         ymax = AVG + SEM,
+                                                         fill = attrib)) +
+              ggplot2::facet_wrap(~index) +
+              ggplot2::geom_bar(stat = "identity", width = 0.8) + #, position = "dodge") +
+              ggplot2::geom_errorbar(width = 0.4) + #, position = "dodge") +
+              ggplot2::guides(fill = FALSE) +
+              ggplot2::theme_bw()
+            return(gg)
+          })
+          
 # makes a sweet line plot good for visualizing long timecourse data
-linePlot <- function(dataList) {
-  meta <- dataList[[1]]
-  plotData <- dataList[[2]]
-  
-  # create labels for x axis
-  # tolerate NAs
-  temp <- na.omit(meta)
-  hoursPerIdx <- as.numeric(difftime(temp[2, 2], temp[1, 2], units = "hours"))
-  breakSeq <- seq(0, meta$index[length(meta$index)], 12 / hoursPerIdx)
-  
-  # create the actual plot and return it
-  gg <- ggplot2::ggplot(plotData, ggplot2::aes(x = index,
-                                               y = AVG,
-                                               ymin = AVG - SEM,
-                                               ymax = AVG + SEM,
-                                               fill = attrib,
-                                               color = attrib)) +
-    ggplot2::geom_line() +
-    ggplot2::geom_ribbon(alpha = 0.3, color = NA) +
-    ggplot2::theme_bw() +
-    ggplot2::scale_x_continuous(breaks = breakSeq, labels = breakSeq * hoursPerIdx)
-  
-  # TODO: need to somehow specify this before the colored line calls
-  # ugly, but it works... retrieve maximum y axis value
-  maxVal <- ggplot2::ggplot_build(gg)$panel$ranges[[1]]$y.range[2]
-  meta$light_status <- (1 - meta$light_status) * maxVal            
-  meta$light_status[meta$light_status == 0] <- NA
-  gg <- gg + ggplot2::geom_ribbon(data = meta,
-                                  ggplot2::aes(x = index, y = 0, ymin = 0, ymax = light_status),
-                                  alpha = 0.05, fill = "grey10", color = NA) +
-    ggplot2::scale_y_continuous(limits = c(0, maxVal))
-  
-  return(gg)
-}
+setGeneric("linePlot", function(statsObj) {standardGeneric("linePlot")})
+setMethod("linePlot", signature = "DAMstats", 
+          definition = function(statsObj) {
+            dataList <- prepPlot(statsObj)
+            meta <- dataList[[1]]
+            plotData <- dataList[[2]]
+            
+            # create labels for x axis
+            # tolerate NAs
+            temp <- na.omit(meta)
+            hoursPerIdx <- as.numeric(difftime(temp[2, 2], temp[1, 2], units = "hours"))
+            breakSeq <- seq(0, meta$index[length(meta$index)], 12 / hoursPerIdx)
+            
+            # create the actual plot and return it
+            gg <- ggplot2::ggplot(plotData, ggplot2::aes(x = index,
+                                                         y = AVG,
+                                                         ymin = AVG - SEM,
+                                                         ymax = AVG + SEM,
+                                                         fill = attrib,
+                                                         color = attrib)) +
+              ggplot2::geom_line() +
+              ggplot2::geom_ribbon(alpha = 0.3, color = NA) +
+              ggplot2::theme_bw() +
+              ggplot2::scale_x_continuous(breaks = breakSeq, labels = breakSeq * hoursPerIdx)
+            
+            # TODO: need to somehow specify this before the colored line calls
+            # ugly, but it works... retrieve maximum y axis value
+            maxVal <- ggplot2::ggplot_build(gg)$panel$ranges[[1]]$y.range[2]
+            meta$light_status <- (1 - meta$light_status) * maxVal            
+            meta$light_status[meta$light_status == 0] <- NA
+            gg <- gg + ggplot2::geom_ribbon(data = meta,
+                                            ggplot2::aes(x = index, y = 0, ymin = 0, ymax = light_status),
+                                            alpha = 0.05, fill = "grey10", color = NA) +
+              ggplot2::scale_y_continuous(limits = c(0, maxVal))
+            
+            return(gg)
+          })
