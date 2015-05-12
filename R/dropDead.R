6b067fcc43e374f384dcc2eaca953dd96ba12a58
Jeff Stafford <jeff.stafford@live.com>
2015-05-11 19:05:52 -0700

    catch if no animals died

--- R/dropDead.R
+++ R/dropDead.R
@@ -5,6 +5,11 @@ setMethod("dropDead", signature = "DAM",
           definition = function(obj) {
             survival <- calcSurvivalTime(obj)
             toDrop <- names(which(!is.na(survival)))
-            print(paste("Animals", toDrop, "have been detected as dead and will be dropped."))
-            return(dropAnimals(obj, toDrop))
+            if (length(toDrop) > 0) {
+              print(paste("Vial #", toDrop, "have been detected as dead and will be dropped."))
+              return(dropAnimals(obj, toDrop))
+            } else {
+              print("No animals detected as dead.")
+              return(obj)
+            }
           })
