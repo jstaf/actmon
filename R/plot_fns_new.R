8e89117205b3228279bd80eaf86e1d51b89742cb
Jeff Stafford <jeff.stafford@live.com>
2015-06-12 18:32:43 -0700

    starting commit busride

--- R/plot_fns_new.R
+++ R/plot_fns_new.R
@@ -1,7 +1,7 @@
 # new plotting functions that use a sensible data format
 
-setGeneric("barPlot2", def = function(obj, attribute) standardGeneric("barPlot2"))
-setMethod("barPlot2", signature = c("DAM", "character"), 
+setGeneric("prepPlot2", def = function(obj, attribute) {standardGeneric("prepPlot2")})
+setMethod("prepPlot2", signature = c("DAM", "character"),
           definition = function(obj, attribute) {
             dat <- toTidy(obj)
             plotData <- plyr::ddply(dat, c(attribute, "read_index"), plyr::summarise,
@@ -10,6 +10,13 @@ setMethod("barPlot2", signature = c("DAM", "character"),
             colnames(plotData)[1] <- "attr"
             # convert read_index to hours
             plotData$read_index <- plotData$read_index * getInterval(obj) / 3600
+            return(plotData)
+          })
+
+setGeneric("barPlot2", def = function(obj, attribute) {standardGeneric("barPlot2")})
+setMethod("barPlot2", signature = c("DAM", "character"), 
+          definition = function(obj, attribute) {
+            plotData <- prepPlot2(obj, attribute)
             
             # create the plot
             gg <- ggplot2::ggplot(data = plotData, ggplot2::aes(x = attr,
@@ -22,5 +29,40 @@ setMethod("barPlot2", signature = c("DAM", "character"),
               ggplot2::geom_errorbar(width = 0.4) +
               ggplot2::guides(fill = FALSE) + xlab("") + # erase redundant labels
               ggplot2::theme_bw()
+            return(gg)
+          })
+
+setGeneric("linePlot2", def = function(obj, attribute) {standardGeneric("linePlot2")})
+setMethod("linePlot2", signature = c("DAM", "character"),
+          definition = function(obj, attribute) {
+            plotData <- prepPlot2(obj, attribute)
+            
+            #TODO copy pasted... need to fix
+            temp <- na.omit(meta)
+            hoursPerIdx <- as.numeric(difftime(temp[2, 2], temp[1, 2], units = "hours"))
+            breakSeq <- seq(0, meta$index[length(meta$index)], 12 / hoursPerIdx)
+            
+            #TODO copy pasted... need to fix
+            gg <- ggplot2::ggplot(plotData, ggplot2::aes(x = index,
+                                                         y = AVG,
+                                                         ymin = AVG - SEM,
+                                                         ymax = AVG + SEM,
+                                                         fill = attrib,
+                                                         color = attrib)) +
+              ggplot2::geom_line() +
+              ggplot2::geom_ribbon(alpha = 0.3, color = NA) +
+              ggplot2::theme_bw() +
+              ggplot2::scale_x_continuous(breaks = breakSeq, labels = breakSeq * hoursPerIdx)
+            
+            # TODO: need to somehow specify this before the colored line calls
+            # ugly, but it works... retrieve maximum y axis value
+            maxVal <- ggplot2::ggplot_build(gg)$panel$ranges[[1]]$y.range[2]
+            meta$light_status <- (1 - meta$light_status) * maxVal            
+            meta$light_status[meta$light_status == 0] <- NA
+            gg <- gg + ggplot2::geom_ribbon(data = meta,
+                                            ggplot2::aes(x = index, y = 0, ymin = 0, ymax = light_status),
+                                            alpha = 0.05, fill = "grey10", color = NA) +
+              ggplot2::scale_y_continuous(limits = c(0, maxVal))
+            
             return(gg)
           })
