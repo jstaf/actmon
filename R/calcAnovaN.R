267a736b0dc07a7bc596e5e1b1fda2c31cbce334
Jeff Stafford <jeff.stafford@live.com>
2015-06-12 21:18:44 -0700

    calcANOVA() convenience function added to replace calcStats()

--- R/calcAnovaN.R
+++ R/calcAnovaN.R
@@ -13,7 +13,8 @@
 #' @export
 #' 
 #' @examples
-#' activity <- toInterval(DAM_DD, 1, units = "hours", aggregateBy = "sum")
+#' activity <- toInterval(DAM_DD, 12, units = "hours", aggregateBy = "sum")
+#' activity <- toAvgDay(activity)
 #' statsAct <- calcStats(activity, "genotype")
 setGeneric("calcANOVA", function(obj, attribute) {standardGeneric("calcANOVA")})
 setMethod("calcANOVA", signature = "DAM",
@@ -31,52 +32,34 @@ setMethod("calcANOVA", signature = "DAM",
 setGeneric("calcAnova1", function(obj, row, attribute) {standardGeneric("calcAnova1")})
 setMethod("calcAnova1", signature = c("DAM", "numeric", "character"), 
           definition = function(obj, row, attribute) {
-            dat <- getVals(obj@data)[1, ]
+            dat <- getVals(obj@data)[row, ]
             attributes <- obj@sample_info[, attribute]
             if (length(dat) != length(attributes)) {
               stop("Number of conditions is unequal to the data length.")
             }
             model <- aov(dat ~ attributes)
             print(summary(model))
-            # this is dirty - perform tukey hsd if p<0.05
-            if (summary(model)[[1]][["Pr(>F)"]][[1]] <= 0.05) {
-              # grab p values
-              pvals <- TukeyHSD(model)[[1]][, 4]
-              pvals <- pvals[pvals <= 0.05]
-              # not even sure this is necessary, but catch any 0-length pval vectors
-              if (length(pvals) == 0) {
-                pvals <- NA
-              }
-            } else {
-              pvals <- NA
-            }
-            print(pvals)
-            return(pvals)
+
+            return(model)
           })
 
+# this is the internal function used to call 2 way anovas
 setGeneric("calcAnova2", function(obj, attribute) {standardGeneric("calcAnova2")})
 setMethod("calcAnova2", signature = c("DAM", "character"), 
           definition = function(obj, attribute) { 
-            # format data to be useable
-            dat <- getVals(obj@data)
-            conditions <- obj@sample_info[, attribute]
-            time <- obj@data$read_index
-            dimnames(dat) <- list(time, conditions)
-            melted <- reshape2::melt(dat)
-            colnames(melted) <- c("index", "attribute", "value")
-            melted$index <- as.factor(melted$index)
+            melted <- toTidy(obj)
+            col <- which(colnames(melted) == attribute)
+            colnames(melted)[col] <- "attr"
+            melted$read_index <- as.factor(melted$read_index)
             
-            pvals <- NA
-            if (length(unique(melted$index)) <= 6) {
-              # okay compute the model
-              model <- with(melted, aov(value ~ attribute * index))
-              print(summary(model))
-              
-              pvals <- do.call(rbind, TukeyHSD(model))
-              # take only pvals <= 0.05
-              pvals <- pvals[pvals[, "p adj"] <= 0.05, ]
-              print(pvals)
-            }
-            return(pvals)
+            # okay compute the model
+            model <- with(melted, aov(value ~ attr * read_index))
+            print(summary(model))
+            
+            return(model)
           })
 
+# pvals <- do.call(rbind, TukeyHSD(model))
+# # take only pvals <= 0.05
+# pvals <- pvals[pvals[, "p adj"] <= 0.05, ]
+# print(pvals)
